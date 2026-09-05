197a3219d44b92bef3920936d31747ab9b42e26c
Plum-afk <justinlequigan06@gmail.com>
2026-09-06 02:52:55 +0800

    fix: allow CDN avatar images

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "avatars.githubusercontent.com",
       },
+      {
+        protocol: "https",
+        hostname: "cdn.jsdelivr.net",
+      },
     ],
   },
 };
